6d7bcca55a24d9f424c5a5cb7bd4179e9032a69b
LaurinMadelau <85069868+LaurinMadelau@users.noreply.github.com>
2024-11-03 06:57:00 +0700

    Update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,13 @@
 module.exports = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'tugas',
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'tugas',
     dialect: 'mysql',
     pool: {
-        min:0,
-        max:5,
+        min: 0,
+        max: 5,
         acquire: 30000,
         idle: 10000,
     }
-}
+};
