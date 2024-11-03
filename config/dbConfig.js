4fb32147bea8174a1e6b7b0eac5cebc702360d82
LaurinMadelau <85069868+LaurinMadelau@users.noreply.github.com>
2024-11-03 20:41:08 +0700

    Update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,6 +4,7 @@ module.exports = {
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     pool: {
         min:0,
