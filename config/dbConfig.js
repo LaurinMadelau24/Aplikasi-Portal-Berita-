82bf117c8e19d5498e677949a01b1e900739f912
LaurinMadelau <85069868+LaurinMadelau@users.noreply.github.com>
2024-11-03 15:54:30 +0700

    Update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,13 @@
-const mysql = require('mysql2')
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
-
-module.exports = pool.promise()
+module.exports = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'tugas',
+    dialect: 'mysql',
+    pool: {
+        min:0,
+        max:5,
+        acquire: 30000,
+        idle: 10000,
+    }
+}
