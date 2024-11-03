3fe3a6037dedbdbdbc3c0dab6d372aed3ead25ef
LaurinMadelau <85069868+LaurinMadelau@users.noreply.github.com>
2024-11-03 17:39:27 +0700

    Update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,12 @@
-// config/dbConfig.js
 module.exports = {
-    host: process.env.DB_HOST, // Dapatkan dari environment variable
-    user: process.env.DB_USERNAME, // Dapatkan dari environment variable
-    password: process.env.DB_PASSWORD, // Dapatkan dari environment variable
-    database: process.env.DB_DBNAME, // Dapatkan dari environment variable
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'tugas',
     dialect: 'mysql',
     pool: {
-        min: 0,
-        max: 5,
+        min:0,
+        max:5,
         acquire: 30000,
         idle: 10000,
     }
