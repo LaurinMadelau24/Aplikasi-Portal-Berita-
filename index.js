e5e446597128d56264542896d7504217791d7389
LaurinMadelau <85069868+LaurinMadelau@users.noreply.github.com>
2024-11-03 21:14:35 +0700

    Update

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT;
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-app.use(cors()); 
+app.use(cors({
+  origin: 'https://tugasexpress-production.up.railway.app/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+})); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
